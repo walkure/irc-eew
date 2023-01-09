58c95d4afa010495739c5ff897be2dfd74894242
walkure <walkure@3pf.jp>
2023-01-09 17:42:40 +0900

    vscode does not merge existing gitconfig https://github.com/microsoft/vscode-remote-release/issues/6810

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -8,8 +8,3 @@ fi
 if [ ! -e "${LIBDIR}/Decoder.pm" ]; then
     /usr/bin/curl -o "${LIBDIR}/Decoder.pm" https://raw.githubusercontent.com/walkure/Earthquake_EEW_Decoder/master/lib/Earthquake/EEW/Decoder.pm
 fi
-
-git config --global core.autocrlf input
-# ${containerWorkspaceFolder} is same as `pwd`
-# https://github.com/devcontainers/cli/issues/98
-git config --global --add safe.directory $(pwd)
